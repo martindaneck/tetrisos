e63b75c16388334cc75b68dc9e91d5c90f884fcd
martinfallow <m.danek08@seznam.cz>
2026-05-22 17:58:20 +0200

    implement keyboard active polling

--- kernel.c
+++ kernel.c
@@ -1,9 +1,8 @@
 #include <stdbool.h>
 #include <stddef.h>
 #include <stdint.h>
-__attribute__((section(".multiboot"))) struct {
 
-} multiboot_header;
+#include "keyboardap.h"
 
 typedef struct {
     uint32_t flags;
@@ -52,18 +51,70 @@ struct multiboot_color
     uint8_t blue;
 };
 
+// common colors
+struct multiboot_color black = {0, 0, 0};
+struct multiboot_color red = {255, 0, 0};
+struct multiboot_color green = {0, 255, 0};
+struct multiboot_color blue = {0, 0, 255};
 
+
+void draw_pixel_fb(uint32_t x, uint32_t y, struct multiboot_color *color, uint64_t fb_address, uint32_t pitch) {
+    *(uint32_t *)(fb_address + y * pitch + x * 4) = color->red << 16 | color->green << 8 | color->blue;
+}
+// pretty macro for it
+#define draw_pixel(x, y, color) draw_pixel_fb(x, y, color, fb_address, pitch)
+
+void draw_rect_fb(uint32_t x, uint32_t y, uint32_t width, uint32_t height, struct multiboot_color *color, uint64_t fb_address, uint32_t pitch) {
+    for (int i = 0; i < height; i++) {
+        for (int j = 0; j < width; j++) {
+            draw_pixel(x + j, y + i, color);
+        }
+    }
+}
+#define draw_rect(x, y, width, height, color) draw_rect_fb(x, y, width, height, color, fb_address, pitch)
 
 void kernel_main(unsigned long addr) 
-{
+{   
+    // multiboot info
     multiboot_info_t *mbi;
     mbi = (multiboot_info_t *)addr;
 
-    // draw some blue
-    struct multiboot_color color = { 0, 0, 255 };
-    for (int i = 0; i < mbi->framebuffer_height; i++) {
-        for (int j = 0; j < mbi->framebuffer_width; j++) {
-            *(uint32_t *)(mbi->framebuffer_addr + i * mbi->framebuffer_pitch + j * 4) = color.red << 16 | color.green << 8 | color.blue;
+    // framebuffer values
+    uint32_t width = mbi->framebuffer_width;
+    uint32_t height = mbi->framebuffer_height;
+    uint32_t pitch = mbi->framebuffer_pitch;
+    uint64_t fb_address = mbi->framebuffer_addr;
+    
+    int r = 255, g = 0, b = 0;
+    struct multiboot_color pink = {255, 0, 255};
+    
+    kbdap_init();
+    while (true) {
+        /// POLL KEYBOARD
+        kbdap_loop();
+        char c = kbd_US[kbdap_last_scancode];
+        switch (c) {
+            case 'q': if (r < 255) r += 1; break;
+            case 'a': if (r > 0)   r -= 1; break;
+            case 'w': if (g < 255) g += 1; break;
+            case 's': if (g > 0)   g -= 1; break;
+            case 'e': if (b < 255) b += 1; break;
+            case 'd': if (b > 0)   b -= 1; break;
         }
+
+        /// LOGIC
+        struct multiboot_color color = {r, g, b};
+
+        /// RENDER
+        // fill the screen
+        for (int y = 0; y < height; y++) {
+            for (int x = 0; x < width; x++) {
+                draw_pixel(x, y, &color);
+            }
+        }
+        
+        
+        
     }
+        
 }
---
+++ keyboardap.h
@@ -0,0 +1,112 @@
+#include <stdint.h>
+
+#define KBC_STATUS 0x64
+#define KBC_EA 0x60
+
+uint8_t kbdap_last_scancode = 0;
+
+static inline unsigned char inportb (unsigned short _port) {
+    unsigned char rv;
+    asm volatile ("in %1, %0" : "=a" (rv) : "dN" (_port));
+    return rv;
+}
+
+static inline void outportb (unsigned short _port, unsigned char _data) {
+    asm volatile ("out %0, %1" : : "a" (_data), "dN" (_port));
+}
+
+
+void delay() { // temporary delay function until i do PIT or rdtsc
+    for (int i = 0; i < 100000; i++) {
+        asm volatile ("nop");
+    }
+}
+
+void kbdap_send_command(uint8_t command) {
+    while (inportb(KBC_STATUS) & 0x02); // wait for the buffer to be ready
+    outportb(KBC_EA, command);
+}
+
+uint32_t kbdap_get_scancode() {
+    static unsigned e0_code = 0;
+    static unsigned e1_code = 0;
+    static unsigned e1_prev = 0;
+    uint8_t scancode = 0;
+    if (inportb(KBC_STATUS) & 1) { // a scancode is available in the buffer
+        scancode = inportb(KBC_EA);
+        if (e0_code == 1) { // scancode is an e0 code
+            e0_code = 0;
+            return (0xe000 | scancode);
+            } else if (e1_code == 1) { // scancode is first byte of e1 code
+            e1_prev = scancode;
+            e1_code = 2;
+            } else  if (e1_code == 2) { // scancode is second byte of e1 code (first is in e1_prev)
+            e1_code = 0;
+            return (0xe10000 | e1_prev << 8 | scancode);
+            } else if (scancode == 0xe0) {
+            e0_code = 1;
+            scancode = 0;
+            } else if (scancode == 0xe1) {
+            e1_code = 1;
+            scancode = 0;
+        }
+    }
+    return scancode;
+}
+
+
+void kbdap_init() {
+    // empty keyboard buffer
+    while (inportb(KBC_STATUS) & 0x01) {
+        inportb(KBC_EA);
+    }
+    // activate keyboard
+    kbdap_send_command(0xF4);
+    while (inportb(KBC_STATUS) & 0x01) {
+        inportb(KBC_EA);
+    }
+    // self test
+    kbdap_send_command(0xEE);
+}
+
+void kbdap_loop() {
+    uint32_t scancode = kbdap_get_scancode();
+    if (scancode != 0) {
+        kbdap_last_scancode = scancode;
+    }
+    delay();
+}
+
+char kbd_US [128] = {
+    0,  27, '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', '\b',
+  '\t', /* <-- Tab */
+  'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', '[', ']', '\n',
+    0, /* <-- control key */
+  'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';', '\'', '`',  0, '\\', 'z', 'x', 'c', 'v', 'b', 'n', 'm', ',', '.', '/',   0,
+  '*',
+    0,  /* Alt */
+  ' ',  /* Space bar */
+    0,  /* Caps lock */
+    0,  /* 59 - F1 key ... > */
+    0,   0,   0,   0,   0,   0,   0,   0,
+    0,  /* < ... F10 */
+    0,  /* 69 - Num lock*/
+    0,  /* Scroll Lock */
+    0,  /* Home key */
+    0,  /* Up Arrow */
+    0,  /* Page Up */
+  '-',
+    0,  /* Left Arrow */
+    0,
+    0,  /* Right Arrow */
+  '+',
+    0,  /* 79 - End key*/
+    0,  /* Down Arrow */
+    0,  /* Page Down */
+    0,  /* Insert Key */
+    0,  /* Delete Key */
+    0,   0,   0,
+    0,  /* F11 Key */
+    0,  /* F12 Key */
+    0,  /* All other keys are undefined */
+};
